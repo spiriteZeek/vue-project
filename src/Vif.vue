d528006195c069f66a4fa6cbdd3022c8080b639d
spiriteZeek <zeek323474@gmail.com>
2023-12-10 22:48:24 +0800

    feat:v-if,v-show使用方法

---
+++ src/Vif.vue
@@ -0,0 +1,26 @@
+<script>
+import {ref} from 'vue';
+
+const people = ref('女')
+
+export default {
+  name: 'VifComp',
+  setup() {
+    function isBoy() {
+      return people.value === '男';
+    };
+    return {
+      people,
+      isBoy,
+    }
+  }
+}
+</script>
+
+<template>
+  <div v-if="people === '男'">zeek</div>
+  <div v-else>jet</div>
+
+  <div v-show="isBoy()">zeek</div>
+  <div v-show="!isBoy()">jet</div>
+</template>
