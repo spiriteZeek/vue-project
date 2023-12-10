6a07b8630c93741b606cbdb67644a682e9700004
spiriteZeek <zeek323474@gmail.com>
2023-12-10 22:47:49 +0800

    feat:v-model使用方法

---
+++ src/Vd.vue
@@ -0,0 +1,29 @@
+<script>
+import { ref } from "vue";
+const inputValue = ref("123456");
+const modelValue = ref('spirite');
+
+export default {
+  setup() {
+    function printInput(e) {
+      // inputValue.value = e.target.value;
+      console.log(e);
+    }
+
+    setInterval(() => {
+      console.log(modelValue.value);
+    }, 2000)
+
+    return {
+      inputValue,
+      printInput,
+      modelValue,
+    };
+  },
+};
+</script>
+
+<template>
+  <input v-bind:value="inputValue" v-on:input="printInput($event)" />
+  <input v-model="modelValue" />
+</template>
