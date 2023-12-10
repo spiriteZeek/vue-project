7771bab3f087ba2e63d165dee2cb88cabc7b00f8
spiriteZeek <zeek323474@gmail.com>
2023-12-10 12:08:44 +0800

    feat:vue获取dom使用ref

---
+++ src/Time.vue
@@ -0,0 +1,25 @@
+<script>
+import { onMounted, onUpdated, ref } from "vue";
+import { count } from "@/utils/time.js";
+export default {
+  setup() {
+    const display = ref(null);
+    onMounted(() => {
+      console.log(display.value);
+      console.log("onMounted");
+    });
+
+    onUpdated(() => {
+      console.log("onUpdated");
+    });
+
+    return {
+      display,
+      count,
+    };
+  },
+};
+</script>
+<template>
+  <div ref="display">{{ count }}</div>
+</template>
