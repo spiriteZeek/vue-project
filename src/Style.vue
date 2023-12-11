a79a43db22ab9254a4a07eaec921dab663076b80
spiriteZeek <zeek323474@gmail.com>
2023-12-11 15:04:05 +0800

    feat:输入颜色，修改字体颜色

---
+++ src/Style.vue
@@ -0,0 +1,27 @@
+<script>
+import { ref } from "vue";
+
+const inputColor = ref('');
+const colorFont = ref('');
+function changeColor() {
+  alert("change color");
+  colorFont.value = inputColor.value;
+}
+
+export default {
+  name: "StyleComp",
+  setup() {
+    return {
+      inputColor,
+      changeColor,
+      colorFont
+    };
+  },
+};
+</script>
+<template>
+  <div :style="{color: colorFont}">请输入你想要修改的颜色</div>
+  <input v-model="inputColor" />
+  <button @click="changeColor">改色</button>
+</template>
+<style></style>
