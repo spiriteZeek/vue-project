0998f20bb034738afb093401568da59431993193
spiriteZeek <zeek323474@gmail.com>
2023-12-12 11:20:38 +0800

    feat:开始学习

--- src/Style.vue
+++ src/Style.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 
 const inputColor = ref('');
 const colorFont = ref('');
+const bgColor = ref('');
 function changeColor() {
   alert("change color");
   colorFont.value = inputColor.value;
@@ -14,7 +15,8 @@ export default {
     return {
       inputColor,
       changeColor,
-      colorFont
+      colorFont,
+      bgColor,
     };
   },
 };
@@ -23,5 +25,14 @@ export default {
   <div :style="{color: colorFont}">请输入你想要修改的颜色</div>
   <input v-model="inputColor" />
   <button @click="changeColor">改色</button>
+  <br/>
+  <input type="color" v-model="bgColor"/>
+  <div class="box" :style="{backgroundColor: bgColor}">自由选择你想填充的颜色</div>
 </template>
-<style></style>
+<style>
+.box {
+  height: 100px;
+  width: 100px;
+  border: 5px solid red;
+}
+</style>
